44c568e109b0a8f1a4bf85cedc9e2701dc4b4ca4
Alexis <aflopez87@gmail.com>
2025-06-24 21:21:41 -0400

    first commit

---
+++ unit03/block25/codekata.js
@@ -0,0 +1,12 @@
+function nextBigger(n){
+//   convert number to string
+  const numStr = n.toString();
+//   make string into an array of characters
+  const stringArr = numStr.split(" ");
+//   compare each character from right to left
+  for (let i = 0; i < stringArr.length; i++) {}
+//   take the greater character and move to the left while moving all other lesser characters to the right
+//   combine characters into a string
+//   convert string into an number
+  
+}
